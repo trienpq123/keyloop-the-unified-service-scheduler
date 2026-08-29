7346ccbecda37c13e96c6ffbe34cafd86f1424d7
Tcode <anhtrienpq123@gmail.com>
2026-08-29 22:48:45 +0700

    feat: update route and init route

--- fe/keyloop-service-schedule-web/src/app/router.tsx
+++ fe/keyloop-service-schedule-web/src/app/router.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router";
 
-// 1. Define placeholder components (or import your own pages)
 const Home = () => <h1>Home Page</h1>;
 const About = () => <h1>About Page</h1>;
 
-// 2. Define the route paths and structural hierarchy 
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />,
@@ -17,10 +13,3 @@ const router = createBrowserRouter([
         element: <About />,
     },
 ]);
-
-// 3. Render the application inside the provider
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-);
--- fe/keyloop-service-schedule-web/src/main.tsx
+++ fe/keyloop-service-schedule-web/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import { router } from './app/router'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);    
